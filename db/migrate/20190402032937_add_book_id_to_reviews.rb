0b045d9548ea5b879bace4308a0f1adf9cf46901
Khanh Nguyen <khanh.nguyen22784@gmail.com>
2019-04-17 15:13:55 +0700

    black application for tutorial (#6)

--- db/migrate/20190402032937_add_book_id_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddBookIdToReviews < ActiveRecord::Migration[5.2]
-  def change
-    add_column :reviews, :book_id, :integer
-  end
-end
--- db/migrate/20190414090901_create_messengers.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessengers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :messengers do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
