3aff3fad882f3ebf041a57d63999f19433833bb0
hung <bibochip@gmail.com>
2019-04-21 17:26:22 +0700

    Issue-2: Create Product Management

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :quatity, presence: true
+end
