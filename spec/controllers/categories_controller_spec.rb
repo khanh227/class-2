45ffd4d2947ec405c2a721a3b35394e0120499b5
hung <bibochip@gmail.com>
2019-04-29 20:35:56 +0700

    Issue-12-create-action: Spec for Product Category/Create

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -20,4 +20,30 @@ RSpec.describe CategoriesController, type: :controller do
       expect(response).to render_template(:new)
     end
   end
+
+  describe 'POST #create' do
+    let(:params) do
+      {
+        name: 'name_3',
+        enabled: false
+      }
+    end
+    context 'success' do
+      specify do
+        expect{ post :create, params: {category: params} }.
+        to change(Category, :count).by(1)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(categories_path)
+      end
+    end
+    context 'failure' do
+      specify do
+        allow_any_instance_of(Category).to receive(:save).and_return(false)
+        expect{ post :create, params: {category: params} }.
+        not_to change(Category, :count)
+        expect(response).not_to have_http_status(302)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
