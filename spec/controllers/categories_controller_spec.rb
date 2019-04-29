8347c6a2fa1c16143f0f5b3819383e4ab69972b0
hung <bibochip@gmail.com>
2019-04-29 16:21:19 +0700

    Issue-12: Spec for Product Category/Index

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :controller do
+  let!(:category_1) { create(:category) }
+  let!(:category_2) { create(:category) }
+
+  describe 'GET index' do
+    specify do
+      get :index
+      expect(assigns(:categories)).to eq [category_1, category_2]
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+    end
+  end  
+end
