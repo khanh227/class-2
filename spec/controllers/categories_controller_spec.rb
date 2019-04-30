ed8a0eef17a1419b9eedc7508274fdc63eb50100
HungJR <bibochip@gmail.com>
2019-04-30 14:14:26 +0700

    Issue-12-update-action: Spec for Product Category/Update (#51)

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe CategoriesController, type: :controller do
     end
   end
 
-  describe 'GET #new' do
+  describe 'GET new' do
     specify do
       get :new
       expect(response).to have_http_status(200)
@@ -21,10 +21,10 @@ RSpec.describe CategoriesController, type: :controller do
     end
   end
 
-  describe 'POST #create' do
+  describe 'POST create' do
     let(:params) do
       {
-        name: 'name_3',
+        name: 'category 3',
         enabled: false
       }
     end
@@ -35,7 +35,7 @@ RSpec.describe CategoriesController, type: :controller do
           post :create, params: {category: params}
         end.to change(Category, :count).by(1)
         category = Category.last
-        expect(category.name).to eq 'name_3'
+        expect(category.name).to eq 'category 3'
         expect(category.enabled).to eq false
         expect(response).to redirect_to(categories_path)
       end
@@ -51,6 +51,41 @@ RSpec.describe CategoriesController, type: :controller do
           post :create, params: {category: params}
         end.not_to change(Category, :count)
         expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'PATCH update' do
+    let(:params) do
+      {
+        name: 'category 4',
+        enabled: true
+      }
+    end
+
+    context 'success' do
+      before {
+        patch :update, params: {category: params, id: category_1.id}
+      }
+
+      specify do
+        category_1.reload
+        expect(category_1.name).to eq('category 4')
+        expect(category_1.enabled).to eq(true)
+        expect(response).to redirect_to(categories_path)
+      end
+    end
+
+    context 'failure' do
+      before {
+        allow_any_instance_of(Category).to receive(:update).and_return(false)
+      }
+
+      specify do
+        expect do
+          patch :update, params: {category: params, id: category_1.id}
+        end.not_to change {category_1}
+        expect(response).to render_template(:edit)
       end
     end
   end
