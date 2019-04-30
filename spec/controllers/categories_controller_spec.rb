8fdb84a61864d1c8b4c2668ed8621d57477a5da2
hung <bibochip@gmail.com>
2019-04-30 08:56:38 +0700

    update code

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -30,17 +30,21 @@ RSpec.describe CategoriesController, type: :controller do
     end
     context 'success' do
       specify do
-        expect{ post :create, params: {category: params} }.
-        to change(Category, :count).by(1)
+        expect do
+          post :create, params: {category: params}
+          end.to change(Category, :count).by(1)
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(categories_path)
+        expect(Category.last.name).to eq 'name_3'
+        expect(Category.last.enabled).to eq false
       end
     end
     context 'failure' do
       specify do
         allow_any_instance_of(Category).to receive(:save).and_return(false)
-        expect{ post :create, params: {category: params} }.
-        not_to change(Category, :count)
+        expect do
+          post :create, params: {category: params}
+          end.not_to change(Category, :count)
         expect(response).not_to have_http_status(302)
         expect(response).to render_template(:new)
       end
