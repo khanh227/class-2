036c76d75814c28a233ce22d9a523f03fa5252ad
luongcongdai <lcongdai@gmail.com>
2019-04-30 10:43:05 +0700

    remove conflict

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -27,43 +27,40 @@ RSpec.describe ProductsController, type: :controller do
     end
   end 
 
-  describe 'GET #create' do
+
+  describe 'PATH #update' do
     before { sign_in user_1 }
 
     let(:params) do
     {
-      product: {
-        name: 'Product',
-        description: 'Product Description',
-        price: 10000,
-        quatity: 1
-      }
+      name: 'Product 2',
+      description: 'Product Description',
+      price: 15000,
+      quatity: 2
     }
     end
 
     context 'success' do
       specify do
         expect do
-          post :create, params: params
-        end.to change(Product, :count).by(1)
-        product = Product.last
-        expect(product.name).to eq 'Product'
-        expect(product.description).to eq 'Product Description'
-        expect(product.price).to eq 10000
-        expect(product.quatity).to eq 1
+          patch :update, params: { product: params, id: product_1.id }
+          expect(Product.find(product_1[:id])[:name]).to eq 'Product 2'
+          expect(Product.find(product_1[:id])[:description]).to eq 'Product Description'
+          expect(Product.find(product_1[:id])[:price]).to eq 15000
+          expect(Product.find(product_1[:id])[:quatity]).to eq 2
+          expect(response).to redirect_to product_path(product_1)
+        end
       end
     end
 
     context 'failure' do
-      before { 
-        allow_any_instance_of(Product).to receive(:save).and_return(false) 
-      }
-
+      before { allow_any_instance_of(Product).to receive(:update).and_return(false) }
+      
       specify do
-        expect do
-          post :create, params: params
-        end.not_to change { Product }
-      end 
+        patch :update, params: {product: params, id: product_1.id}
+        expect(response).to render_template(:edit)
+      end
     end
   end
 end
+ 
