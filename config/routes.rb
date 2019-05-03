cd913b676d1146aa07cdca4661ffd38ad804d24c
luongcongdai <lcongdai@gmail.com>
2019-05-03 08:28:03 +0700

    update code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :products 
   devise_for :users
   resources :categories
-  resources :products 
   resources :product_imports do
     collection { post :import }
   end
