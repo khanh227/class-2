cfd2cb4ee8fa4246e51112ac8192a6a737b0730c
luongcongdai <lcongdai@gmail.com>
2019-05-06 16:35:08 +0700

    Issue-6: Import Products

---
+++ spec/services/importor_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require 'csv'
+
+describe 'Importor' do
+  let(:file){ File.new('spec/fixtures/products.csv', headers: true) }
+  let(:params) do
+    {
+      product: {
+        name: 'áo thun 1',
+        description: 'description 1',
+        price: 34000,
+        quatity: 3
+      }
+    }
+  end
+
+  before { @file_data = params[:file] }
+
+  context 'success' do
+    specify do
+      expect do
+      end
+    end
+  end
+end
