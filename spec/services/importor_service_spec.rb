ad101647b8a790cac3a287c465404c99e85637e4
luongcongdai <lcongdai@gmail.com>
2019-05-06 21:26:22 +0700

    update code

--- spec/services/importor_service_spec.rb
+++ spec/services/importor_service_spec.rb
@@ -9,11 +9,11 @@ describe 'Importor' do
         name: 'áo thun 1',
         description: 'description 1',
         price: 34000,
+        enabled: 'true',
         quatity: 3
       }
     }
   end
-
   before { @file_data = params[:file] }
 
   context 'success' do
