2932f7f35437042ed4cd9671187d737da92542ec
khanh227 <khanh.nguyen22784@gmail.com>
2019-05-07 21:52:53 +0700

    update code

--- spec/services/products/importor_spec.rb
+++ spec/services/products/importor_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 describe Products::Importor do
   let(:file){ File.new('spec/fixtures/products.csv', headers: true) }
-  let!(:service) { Products::Importor.new(file_import: file).perform }
-  
-  context 'perform success' do
-    specify do
-      expect(service).to eq true
-      expect(service).to change(Product, :count).by(2)
+  let(:service) { Products::Importor.new(file_import: file).perform }
+
+  describe '#perform' do
+    context 'success' do
+      specify do
+        expect do 
+          service
+        end.to change(Product, :count).by(2)
+        expect(Product.last.name).to eq 'iPad WiFi 128GB New 2018'
+        expect(Product.last.description).to eq 'description 2'
+        expect(Product.last.price).to eq 0.98e5
+        expect(Product.last.enabled).to eq true
+        expect(Product.last.quatity).to eq 4
+      end
+    end
+
+    context 'failure' do
+      # continue
     end
   end
 end
