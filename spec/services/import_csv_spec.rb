0b7def77048b98ef00135868d63e1b616486d91e
Luong Cong Dai <49638811+luongcongdai@users.noreply.github.com>
2019-05-08 14:35:46 +0700

    Issue-22: Spec for Import Products (#78)
    * Issue-22: Spec for Import Products

---
+++ spec/services/import_csv_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe ImportCsv do
+  let!(:file) { fixture_file_upload('spec/fixtures/products.csv') }
+  let!(:service) { Products::Importor.new(file_import: file) }
+
+  specify do
+    expect(service.success).to eq false
+    expect(service.messages).not_to eq ''
+    expect(service.csv_valid?).to eq true
+  end
+end
+
