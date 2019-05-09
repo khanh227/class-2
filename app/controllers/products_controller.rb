8f7438822a32c7c14195fe22e6b61c3a45f98a48
hung <bibochip@gmail.com>
2019-05-09 08:43:04 +0700

    update helper

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,6 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @categories = Category.all
   end
 
   def show;end
--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -2,4 +2,8 @@ module ProductHelper
   def user_can_edit_product
     user_signed_in? && @product.user_id == current_user.id
   end
+
+  def category_list
+    Category.all
+  end
 end
