a38c7544b0ce843e54b689c7d441e75ee9e7dab2
hung <bibochip@gmail.com>
2019-05-13 15:09:21 +0700

    update code

--- app/controllers/lunch_orders_controller.rb
+++ app/controllers/lunch_orders_controller.rb
@@ -7,28 +7,6 @@ class LunchOrdersController < ApplicationController
     lunch_order
   end
 
-  def new
-    @lunch_order = LunchOrder.new
-  end
-
-  def edit
-    lunch_order
-  end
-
-  def create
-    @lunch_order = LunchOrder.new(lunch_order_params)
-    @lunch_order.save ? redirect_to(lunch_orders_path) : render('new')
-  end
-
-  def update
-    lunch_order.update(lunch_order_params) ? redirect_to(lunch_orders_path) : render('edit')
-  end
-
-  def destroy
-    lunch_order.destroy
-    redirect_to lunch_orders_path
-  end
-
   private
     def lunch_order
       @lunch_order ||= LunchOrder.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   resources :categories
-  resources :lunch_orders
+  resources :lunch_orders, only: [:index, :show]
   resources :product_imports do
     collection { post :import }
   end
