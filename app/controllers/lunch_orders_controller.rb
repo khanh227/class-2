fbbda1c6eed5c85b27e072c5677d458b43cca0ab
hung <bibochip@gmail.com>
2019-05-14 08:34:09 +0700

    update code

--- app/controllers/lunch_orders_controller.rb
+++ app/controllers/lunch_orders_controller.rb
@@ -4,15 +4,6 @@ class LunchOrdersController < ApplicationController
   end
 
   def show
-    lunch_order
+    @lunch_order ||= LunchOrder.find(params[:id])
   end
-
-  private
-    def lunch_order
-      @lunch_order ||= LunchOrder.find(params[:id])
-    end
-
-    def lunch_order_params
-      params.require(:lunch_order).permit(:order_date)
-    end
 end
