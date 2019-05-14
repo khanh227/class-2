f82ea3e76db634a7eb3cfc565a666773763de7fb
HungJR <bibochip@gmail.com>
2019-05-14 14:36:14 +0700

    Issue-21: Add role_user to User (#91)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role_user: [:customer, :restaurant, :admin]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
