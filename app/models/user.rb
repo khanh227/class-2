b47aca133cd277818bcf9d802b71ffbccee23617
hung <bibochip@gmail.com>
2019-05-14 14:15:56 +0700

    update code

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  enum role_user: [:member, :mod, :admin]
+  enum role_user: [:customer, :restaurant, :admin]
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
