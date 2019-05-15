8992af11ad0e07aabecbfb1361a930ea819bade7
luongcongdai <lcongdai@gmail.com>
2019-05-15 10:40:40 +0700

    Issue-30: Create Home page

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,27 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+$(function () { function moveItems(origin, dest) {
+    $(origin).find(':selected').appendTo(dest);
+}
+ 
+function moveAllItems(origin, dest) {
+    $(origin).children().appendTo(dest);
+}
+ 
+$('#left').click(function () {
+    moveItems('#sbTwo', '#sbOne');
+});
+ 
+$('#right').on('click', function () {
+    moveItems('#sbOne', '#sbTwo');
+});
+ 
+$('#leftall').on('click', function () {
+    moveAllItems('#sbTwo', '#sbOne');
+});
+ 
+$('#rightall').on('click', function () {
+    moveAllItems('#sbOne', '#sbTwo');
+});
+});
