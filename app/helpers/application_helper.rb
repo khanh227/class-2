4d140f3a045289d54c0de184c11d31bef0fc07b6
luongcongdai <lcongdai@gmail.com>
2019-05-16 10:45:21 +0700

    update code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def app_name
     'Lunch Service'
   end
+
+  def display_status(status)
+    icon_class = status 
+    icon_class ? content_tag(:span, nil, class: 'glyphicon glyphicon-ok') : content_tag(:span, nil, class: 'glyphicon glyphicon-remove')
+  end
 end
--- app/helpers/icon_helper.rb
+++
@@ -1,7 +0,0 @@
-module IconHelper
-  def enabled_icon
-    Product.pluck(:enabled).each do |e| 
-      e ? '.glyphicon.glyphicon-ok' : '.glyphicon.glyphicon-remove'
-    end
-  end
-end
