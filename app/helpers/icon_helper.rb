b8da5fa8baa75ab107c24a6057ed60d898f0a9c0
luongcongdai <lcongdai@gmail.com>
2019-05-16 09:48:48 +0700

    update icon helper

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,7 +1,7 @@
 module IconHelper
   def enabled_icon
     Product.pluck(:enabled).each do |e| 
-      e ? 'glyphicon.glyphicon-ok' : 'glyphicon.glyphicon-remove'
+      e ? '.glyphicon.glyphicon-ok' : '.glyphicon.glyphicon-remove'
     end
   end
 end
