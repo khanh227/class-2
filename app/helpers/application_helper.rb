e443539db3fa3b17094098c57a07589309a7fea0
Luong Cong Dai <49638811+luongcongdai@users.noreply.github.com>
2019-05-20 10:14:49 +0700

    Issue-31: Update Display Status Helper (#97)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def app_name
     'Lunch Service'
   end
+
+  def display_status(status)
+    icon_class = status ? 'ok' : 'remove'
+    content_tag(:span, nil, class: "glyphicon glyphicon-#{icon_class}")
+  end
 end
