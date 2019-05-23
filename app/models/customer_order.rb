8f1f0a49870a8ab164e6ae46247a456e0b727b4a
luongcongdai <lcongdai@gmail.com>
2019-05-23 11:21:33 +0700

    update code

--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -5,8 +5,4 @@ class CustomerOrder < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :lunch_order_id }
   
-  def update_attribute(name, value)
-    send(name.to_s + '=', value)
-    #save(false)
-  end
 end
