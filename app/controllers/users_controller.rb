ba08ec8486657e56027c89638e770a286153c26a
HungBui <bibochip@gmail.com>
2019-05-24 13:51:54 +0700

    update users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @users = @users.order(created_at: :desc)
+  end
+
+  def edit
+    @user
+  end
+
+  def show
+    @user
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    redirect_to users_path if @user.destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:role_user)
+    end
+end
