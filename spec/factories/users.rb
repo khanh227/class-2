084b146a688777e0f626ef4b39d45275ef489ffd
khanh227 <khanh.nguyen22784@gmail.com>
2019-05-25 21:43:41 +0700

    update factory user role and spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
     password "@ssword-#{SecureRandom.hex(8)}"
+    role_user :customer
+
+    factory :admin_user do
+      role_user :admin
+    end
+
+    factory :restaurant_user do
+      role_user :restaurant
+    end
+
   end
 end
