d1c02978699d514ce05c10dac0712cecc2375db8
HungBui <bibochip@gmail.com>
2019-05-28 15:43:52 +0700

    update users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe UsersController, type: :controller do
 
   describe 'admin user' do
     let(:user) { create(:admin_user) }
-    let(:user_restaurant) { create(:restaurant_user) }
-    let(:user_customer) { create(:user) }
+    let(:user_1) { create(:restaurant_user) }
+    let(:user_2) { create(:user) }
 
     describe 'GET index' do
       before { get :index }
 
       specify do
-        expect(assigns(:users)).to eq [user, user_restaurant, user_customer]
+        expect(assigns(:users)).to eq [user, user_1, user_2]
         expect(response).to have_http_status(200)
         expect(response).to render_template(:index)
       end
@@ -20,15 +20,15 @@ RSpec.describe UsersController, type: :controller do
 
     describe 'GET show' do
       specify do
-        get :show, params: { id: user_restaurant.id }
+        get :show, params: { id: user_1.id }
         expect(response).to render_template(:show)
-        expect(assigns(:user)).to eq user_restaurant
+        expect(assigns(:user)).to eq user_1
       end
     end
 
     describe 'GET edit' do
       specify do
-        get :edit, params: { id: user_restaurant.id }
+        get :edit, params: { id: user_1.id }
         expect(response).to render_template(:edit)
       end
     end
@@ -36,31 +36,28 @@ RSpec.describe UsersController, type: :controller do
     describe 'PATCH update' do
       let(:params) do
         {
-          role_user: 'customer'
+          user: { role_user: 'customer' },
+          id: user_1.id
         }
       end
 
       context 'success' do
-        before {
-          patch :update, params: { user: params, id: user_restaurant.id }
-        }
+        before { patch :update, params: params }
 
         specify do
-          user_restaurant.reload
-          expect(user_restaurant.role_user).to eq 'customer'
+          user_1.reload
+          expect(user_1.role_user).to eq 'customer'
           expect(response).to redirect_to(user_path)
         end
       end
 
       context 'failure' do
-        before {
-          allow_any_instance_of(User).to receive(:update).and_return(false)
-        }
+        before { allow_any_instance_of(User).to receive(:update).and_return(false) }
 
         specify do
           expect do
-            patch :update, params: { user: params, id: user_customer.id }
-          end.not_to change { user_customer }
+            patch :update, params: params
+          end.not_to change { user_1 }
           expect(response).to render_template(:edit)
         end
       end
@@ -95,14 +92,13 @@ RSpec.describe UsersController, type: :controller do
     describe 'PATCH update' do
       let(:params) do
         {
-          role_user: 'customer'
+          user: { role_user: 'customer' },
+          id: user.id
         }
       end
 
       context 'access denied' do
-        before {
-          patch :update, params: { user: params, id: user.id }
-        }
+        before { patch :update, params: params }
 
         specify do
           expect(response).to have_http_status(403)
@@ -139,14 +135,13 @@ RSpec.describe UsersController, type: :controller do
     describe 'PATCH update' do
       let(:params) do
         {
-          role_user: 'customer'
+          user: { role_user: 'customer' },
+          id: user.id
         }
       end
 
       context 'access denied' do
-        before {
-          patch :update, params: { user: params, id: user.id }
-        }
+        before { patch :update, params: params }
 
         specify do
           expect(response).to have_http_status(403)
