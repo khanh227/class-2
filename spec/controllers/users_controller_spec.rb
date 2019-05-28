c840c642612a14c9b1791110f9c12f86b02eb3bf
HungBui <bibochip@gmail.com>
2019-05-28 10:54:09 +0700

    update spec with roles user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,65 +2,155 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   let!(:user_admin) { create(:user, role_user: :admin) }
-  let!(:user_1) { create(:user, role_user: :restaurant) }
-  let!(:user_2) { create(:user, role_user: :customer) }
+  let!(:user_restaurant) { create(:user, role_user: :restaurant) }
+  let!(:user_customer) { create(:user, role_user: :customer) }
 
-  before { sign_in user_admin }
+  describe 'admin user' do
+    before { sign_in user_admin }
 
-  describe 'GET index' do
-    before { get :index }
-
-    specify do
-      expect(assigns(:users)).to eq [user_admin, user_1, user_2]
-      expect(response).to have_http_status(200)
-      expect(response).to render_template(:index)
-    end
-  end
-
-  describe 'GET show' do
-    specify do
-      get :show, params: { id: user_1.id }
-      expect(response).to render_template(:show)
-      expect(assigns(:user)).to eq user_1
-    end
-  end
-
-  describe 'GET edit' do
-    specify do
-      get :edit, params: { id: user_1.id }
-      expect(response).to render_template(:edit)
-    end
-  end
-
-  describe 'PATCH update' do
-    let(:params) do
-      {
-        role_user: :customer
-      }
-    end
-
-    context 'success' do
-      before {
-        patch :update, params: { user: params, id: user_1.id }
-      }
+    describe 'GET index' do
+      before { get :index }
 
       specify do
-        user_1.reload
-        expect(user_1.role_user).to eq 'customer'
-        expect(response).to redirect_to(user_path)
+        expect(assigns(:users)).to eq [user_admin, user_restaurant, user_customer]
+        expect(response).to have_http_status(200)
+        expect(response).to render_template(:index)
       end
     end
 
-    context 'failure' do 
-      before {
-        allow_any_instance_of(User).to receive(:update).and_return(false)
-      }
+    describe 'GET show' do
+      specify do
+        get :show, params: { id: user_restaurant.id }
+        expect(response).to render_template(:show)
+        expect(assigns(:user)).to eq user_restaurant
+      end
+    end
+
+    describe 'GET edit' do
+      specify do
+        get :edit, params: { id: user_restaurant.id }
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    describe 'PATCH update' do
+      let(:params) do
+        {
+          role_user: 'customer'
+        }
+      end
+
+      context 'success' do
+        before {
+          patch :update, params: { user: params, id: user_restaurant.id }
+        }
+
+        specify do
+          user_restaurant.reload
+          expect(user_restaurant.role_user).to eq 'customer'
+          expect(response).to redirect_to(user_path)
+        end
+      end
+
+      context 'failure' do
+        before {
+          allow_any_instance_of(User).to receive(:update).and_return(false)
+        }
+
+        specify do
+          expect do
+            patch :update, params: { user: params, id: user_customer.id }
+          end.not_to change { user_customer }
+          expect(response).to render_template(:edit)
+        end
+      end
+    end
+  end
+
+  describe 'restaurant user' do
+    before { sign_in user_restaurant }
+
+    describe 'GET index' do
+      before { get :index }
 
       specify do
-        expect do
-          patch :update, params: { user: params, id: user_2.id }
-        end.not_to change { user_2 }
-        expect(response).to render_template(:edit)
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    describe 'GET show' do
+      specify do
+        get :show, params: { id: user_restaurant.id }
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    describe 'GET edit' do
+      specify do
+        get :edit, params: { id: user_restaurant.id }
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    describe 'PATCH update' do
+      let(:params) do
+        {
+          role_user: 'customer'
+        }
+      end
+
+      context 'access denied' do
+        before {
+          patch :update, params: { user: params, id: user_restaurant.id }
+        }
+
+        specify do
+          expect(response).to have_http_status(403)
+        end
+      end
+    end
+  end
+
+  describe 'customer user' do
+    before { sign_in user_customer }
+
+    describe 'GET index' do
+      before { get :index }
+
+      specify do
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    describe 'GET show' do
+      specify do
+        get :show, params: { id: user_restaurant.id }
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    describe 'GET edit' do
+      specify do
+        get :edit, params: { id: user_restaurant.id }
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    describe 'PATCH update' do
+      let(:params) do
+        {
+          role_user: 'customer'
+        }
+      end
+
+      context 'access denied' do
+        before {
+          patch :update, params: { user: params, id: user_restaurant.id }
+        }
+
+        specify do
+          expect(response).to have_http_status(403)
+        end
       end
     end
   end
