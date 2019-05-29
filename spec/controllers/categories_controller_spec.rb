12ec1ace18bbcdc2356978e39787256e35498bac
luongcongdai <lcongdai@gmail.com>
2019-05-29 13:25:43 +0700

    update share examples for spec

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CategoriesController, type: :controller do
   let!(:category_1) { create(:category) }
   let!(:category_2) { create(:category) }
 
-  context 'Admin User' do
+  shared_examples 'CRUD User' do
     let!(:user) { create(:admin_user) }
 
     describe 'GET index' do
@@ -121,118 +121,17 @@ RSpec.describe CategoriesController, type: :controller do
     end
   end
 
-  context 'Restaurant User' do
+  describe 'Admin User' do
+    let!(:user) { create(:admin_user) }
+
+    it_behaves_like 'CRUD User' do
+    end
+  end
+
+  describe 'Restaurant User' do
     let!(:user) { create(:restaurant_user) }
 
-    describe 'GET index' do
-      before { get :index }
-
-      specify do
-        expect(assigns(:categories)).to eq [category_1, category_2]
-        expect(response).to have_http_status(200)
-        expect(response).to render_template(:index)
-      end
-    end
-
-    describe 'GET show' do
-      specify do
-        get :show, params: { id: category_1.id }
-        expect(response).to render_template(:show)
-        expect(assigns(:category)).to eq category_1
-      end
-    end
-
-    describe 'GET new' do
-      specify do
-        get :new
-        expect(response).to have_http_status(200)
-        expect(response).to render_template(:new)
-      end
-    end
-
-    describe 'GET edit' do
-      specify do
-        get :edit, params: { id: category_1.id }
-        expect(response).to render_template(:edit)
-      end
-    end
-
-    describe 'POST create' do
-      let(:params) do
-        {
-          name: 'category 5',
-          enabled: true
-        }
-      end
-
-      context 'success' do
-        specify do
-          expect do
-            post :create, params: { category: params }
-          end.to change(Category, :count).by(1)
-          category = Category.last
-          expect(category.name).to eq 'category 5'
-          expect(category.enabled).to eq true
-          expect(response).to redirect_to categories_path
-        end
-      end
-
-      context 'failure' do
-        before {
-          allow_any_instance_of(Category).to receive(:save).and_return(false)
-        }
-
-        specify do
-          expect do
-            post :create, params: { category: params }
-          end.not_to change(Category, :count)
-          expect(response).to render_template(:new)
-        end
-      end
-    end
-
-    describe 'PATCH update' do
-      let(:params) do
-        {
-          name: 'category 6',
-          enabled: false
-        }
-      end
-
-      context 'success' do
-        before {
-          patch :update, params: { category: params, id: category_1.id }
-        }
-
-        specify do
-          category_1.reload
-          expect(category_1.name).to eq 'category 6' 
-          expect(category_1.enabled).to eq false 
-          expect(response).to redirect_to categories_path
-        end
-      end
-
-      context 'failure' do
-        before {
-          allow_any_instance_of(Category).to receive(:update).and_return(false)
-        }
-
-        specify do
-          expect do
-            patch :update, params: { category: params, id: category_1.id }
-          end.not_to change { category_1 }
-          expect(response).to render_template(:edit)
-        end
-      end
-    end
-
-    describe 'DELETE destroy' do
-      specify do
-        expect do
-          delete :destroy, params: { id: category_1.id }
-        end.to change(Category, :count).by(-1)
-        expect(response).to redirect_to categories_path
-      end
+    it_behaves_like 'CRUD User' do
     end
   end
 
--- spec/controllers/menus_controller_spec.rb
+++ spec/controllers/menus_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe MenusController, type: :controller do
   let!(:menu_2) { create(:menu, product_ids: [product_3.id]) }
 
     
-  context 'Admin User' do
+  shared_examples 'CRUD User' do
     let!(:user) { create(:admin_user) }
 
     describe 'GET index' do
@@ -115,209 +115,17 @@ RSpec.describe MenusController, type: :controller do
     end
   end
 
-  context 'Customer User' do
-    let!(:user) { create(:restaurant_user) }
+  describe 'admin_user User' do
+    let!(:user) { create(:admin_user) }
 
-    describe 'GET index' do
-      before { get :index }
-
-      specify do
-        expect(response).to have_http_status(200) 
-        expect(response).to render_template(:index) 
-        expect(assigns(:menus)).to eq [menu_1, menu_2]
-      end
-    end
-
-    describe 'GET new' do
-      before { get :new }
-
-      specify do
-        expect(response).to have_http_status(200) 
-        expect(response).to render_template(:new)
-      end
-    end 
-
-    describe 'POST create' do
-      let!(:user) { create(:restaurant_user, email: 'testtest@test.com') }
-
-      let(:params) do
-      {
-        menu: {
-          menu_date: '2019-05-25',
-          product_ids: [product_1.id,product_2.id],
-          created_by: user.id
-        }
-      }
-      end
-
-      context 'success' do
-        specify do
-          expect do
-            post :create, params: params
-          end.to change(Menu, :count).by(1)
-          menu = Menu.last
-          expect(menu.menu_date.to_s).to eq '2019-05-25'
-          expect(menu.products.first.name).to eq 'Product 1'
-          expect(menu.products).to eq [product_1, product_2]
-          expect(menu.user.email).to eq 'testtest@test.com'
-        end
-      end
-
-      context 'failure' do
-        before { 
-          allow_any_instance_of(Menu).to receive(:save).and_return(false) 
-        }
-
-        specify do
-          expect do
-            post :create, params: params
-          end.not_to change { Menu }
-        end 
-      end
-    end
-
-    describe 'PATCH update' do
-      let(:params) do
-      {
-        menu: {
-          product_ids: [product_3.id],
-        }
-      }
-      end
-
-      context 'success' do
-        specify do
-          expect do
-            patch :update, params: { menu: params, id: menu_1.id }
-            menu_1.reload
-            expect(menu_1.products.first.name).to eq 'Product 3'
-            expect(menu_1.products).to eq product_3
-          end
-        end
-      end
-
-      context 'failure' do
-        before { allow_any_instance_of(Menu).to receive(:update).and_return(false) }
-        
-        specify do
-          expect do
-            patch :update, params: {menu: params, id: menu_1.id}
-          end.not_to change { menu_1 }
-          expect(response).to render_template(:edit)
-        end
-      end
-    end
-
-    describe 'DELETE destroy' do
-      specify do
-        expect do
-          delete :destroy, params: {id: menu_2.id}
-        end.to change(Menu, :count).by(-1)
-        expect(response).to redirect_to menus_path
-      end
+    it_behaves_like 'CRUD User' do
     end
   end
-  
-  context 'Restaurant User' do
+
+  describe 'Restaurant User' do
     let!(:user) { create(:restaurant_user) }
 
-    describe 'GET index' do
-      before { get :index }
-
-      specify do
-        expect(response).to have_http_status(200) 
-        expect(response).to render_template(:index) 
-        expect(assigns(:menus)).to eq [menu_1, menu_2]
-      end
-    end
-
-    describe 'GET new' do
-      before { get :new }
-
-      specify do
-        expect(response).to have_http_status(200) 
-        expect(response).to render_template(:new)
-      end
-    end 
-
-    describe 'POST create' do
-      let!(:user) { create(:restaurant_user, email: 'test@test.com') }
-
-      let(:params) do
-      {
-        menu: {
-          menu_date: '2019-05-25',
-          product_ids: [product_1.id,product_2.id],
-          created_by: user.id
-        }
-      }
-      end
-
-      context 'success' do
-        specify do
-          expect do
-            post :create, params: params
-          end.to change(Menu, :count).by(1)
-          menu = Menu.last
-          expect(menu.menu_date.to_s).to eq '2019-05-25'
-          expect(menu.products.first.name).to eq 'Product 1'
-          expect(menu.products).to eq [product_1, product_2]
-          expect(menu.user.email).to eq 'test@test.com'
-        end
-      end
-
-      context 'failure' do
-        before { 
-          allow_any_instance_of(Menu).to receive(:save).and_return(false) 
-        }
-
-        specify do
-          expect do
-            post :create, params: params
-          end.not_to change { Menu }
-        end 
-      end
-    end
-
-    describe 'PATCH update' do
-      let(:params) do
-      {
-        menu: {
-          product_ids: [product_3.id],
-        }
-      }
-      end
-
-      context 'success' do
-        specify do
-          expect do
-            patch :update, params: { menu: params, id: menu_1.id }
-            menu_1.reload
-            expect(menu_1.products.first.name).to eq 'Product 3'
-            expect(menu_1.products).to eq product_3
-          end
-        end
-      end
-
-      context 'failure' do
-        before { allow_any_instance_of(Menu).to receive(:update).and_return(false) }
-        
-        specify do
-          expect do
-            patch :update, params: {menu: params, id: menu_1.id}
-          end.not_to change { menu_1 }
-          expect(response).to render_template(:edit)
-        end
-      end
-    end
-
-    describe 'DELETE destroy' do
-      specify do
-        expect do
-          delete :destroy, params: {id: menu_2.id}
-        end.to change(Menu, :count).by(-1)
-        expect(response).to redirect_to menus_path
-      end
+    it_behaves_like 'CRUD User' do
     end
   end
 
