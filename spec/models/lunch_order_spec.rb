ef5a97f615255af6eb3fa9a029f02340436c624f
HungBui <bibochip@gmail.com>
2019-06-04 11:06:52 +0700

    delete 2 line expect in lunch order and product spec

--- spec/models/lunch_order_spec.rb
+++ spec/models/lunch_order_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe LunchOrder, type: :model do
-  describe 'associations' do
-    it { is_expected.to have_many(:customer_orders).dependent(:destroy) }
-  end
+  
 end
