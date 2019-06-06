6daeb66ef30af6726fe2f5a10eb0e0a0372cf0b2
HungBui <bibochip@gmail.com>
2019-06-06 08:51:41 +0700

    update views for menu

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -2,4 +2,13 @@ module MenuHelper
   def menu_list
     Menu.pluck(:menu_name, :id)
   end
+
+  def display_background_img(menu)
+    if menu.background_img.attached?
+      result = url_for(menu.background_img)
+    else
+      result = ''
+    end
+    result
+  end
 end
