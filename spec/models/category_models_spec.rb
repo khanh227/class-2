542fcfe37389426746387f7e01bbd00343e0aedf
nphuonghuy260397 <61397899+nphuonghuy260397@users.noreply.github.com>
2020-02-26 08:25:14 +0700

    Update category_models_spec.rb

--- spec/models/category_models_spec.rb
+++ spec/models/category_models_spec.rb
@@ -1,23 +1,27 @@
 require "rails_helper"
+
 RSpec.describe Category , type: :model do
 
-  context "associations" do
+  describe "associations" do
     it { should have_many (:products)}
   end
 
-  context "validates" do
+  describe "validates" do
     subject {
       described_class.new(name: "Anything",
                           presence: true,
                           length: { minimum: 5 })
+
       it "is valid with valid attributes" do
         expect(subject).to be_valid
       end
-      it "is not valid without a name" do
+
+      it "is invalid due to lack of the name" do
         subject.name = nil
           expect(subject).to_not be_valid
       end
-      it "is not valid without length minimum 5 characters" do
+
+      it "is invalid because the name length is 5 characters or more" do
         subject.length >= 5
           expect(subject).to_not be_valid 
       end
