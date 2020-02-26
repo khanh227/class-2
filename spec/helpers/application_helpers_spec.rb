c1e745dedd4d3431a5fd86422e0acb695595d22b
nphuonghuy260397 <61397899+nphuonghuy260397@users.noreply.github.com>
2020-02-26 14:33:29 +0700

    Update application_helpers_spec.rb

--- spec/helpers/application_helpers_spec.rb
+++ spec/helpers/application_helpers_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 describe ApplicationHelper, type: :helper do
-
   describe '#app_name' do 
-    
-    it{ expect( helper.app_name).to eq "Lunch Service" }
+    it { expect(helper.app_name).to eq 'Lunch Service' }
   end
 
   describe "#display_status" do
-    
     context 'when status not nil'do
-      it{ expect(helper.display_status('abc')).to eq "<span class=\"glyphicon glyphicon-ok\"></span>" }
+      it { expect(helper.display_status('abc')).to eq "<span class=\"glyphicon glyphicon-ok\"></span>" }
     end
     
     context 'when status nil' do
-      it{ expect(helper.display_status(nil)).to eq "<span class=\"glyphicon glyphicon-remove\"></span>" }
+      it { expect(helper.display_status(nil)).to eq "<span class=\"glyphicon glyphicon-remove\"></span>" }
     end
   end
 end
