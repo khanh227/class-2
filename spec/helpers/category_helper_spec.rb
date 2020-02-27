b7bfe501cca8c7740e56ca5c52a0cf5782357c7e
nphuonghuy260397 <61397899+nphuonghuy260397@users.noreply.github.com>
2020-02-27 09:04:46 +0700

    Update category_helper_spec.rb

--- spec/helpers/category_helper_spec.rb
+++ spec/helpers/category_helper_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 RSpec.describe CategoryHelper , type: :helper do
-  let!(:category_1) { create(:category, name: "name 1",enabled: false) }
-  let!(:category_2) { create(:category, name: "name 2",enabled: true) }
-  let!(:category_3) { create(:category, name: "name 3",enabled: false) }
-  let!(:category_4) { create(:category, name: "name 4",enabled: true) }
+  let!(:category_1) { create(:category, name: "name 1", enabled: false) }
+  let!(:category_2) { create(:category, name: "name 2", enabled: true) }
+  let!(:category_3) { create(:category, name: "name 3", enabled: false) }
+  let!(:category_4) { create(:category, name: "name 4", enabled: true) }
 
   describe '#category_list' do
-    it do
-      expect(helper.category_list).to eq [["name 2",category_2.id], ["name 4",category_4.id] ]
-      expect(helper.category_list).not_to eq [["name 1",category_1.id], ["name 3",category_3.id]]
+    specify do
+      expect(helper.category_list).to eq [["name 2",category_2.id],["name 4",category_4.id] ]
+      expect(helper.category_list).not_to eq [["name 1",category_1.id],["name 3",category_3.id]]
     end
   end
 end
